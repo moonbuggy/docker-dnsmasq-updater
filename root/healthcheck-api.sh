b94505b3bfb3d3a47ae8dda582d3ae07a53cb03b
moonbuggy <3319867+moonbuggy@users.noreply.github.com>
2024-12-07 17:32:22 +1100

    better healthcheck for dnsmasq server image

--- root/healthcheck-api.sh
+++ root/healthcheck-api.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/with-contenv /bin/sh
 # shellcheck shell=sh
 
-up="$(s6-svstat -o up /var/run/service/dnsmasq-updater/)"
+dmu_up="$(s6-svstat -o up /var/run/service/dnsmasq-updater/)"
 
 # although we still signal the file descriptor in --manager mode, just like in
 # --standalone mode, it makes more sense to indicate "ready" based on a response
 # from the API
-ready="$(wget -qO- http://127.0.0.1:${DMU_API_PORT:-8080}/status >/dev/null 2>&1 \
+dmu_ready="$(wget -qO- http://127.0.0.1:${DMU_API_PORT:-8080}/status >/dev/null 2>&1 \
 				&& echo 'true' || echo 'false')"
 
-echo "Up: ${up}, Ready: ${ready}"
+echo "Dnsmasq Updater - Up: ${dmu_up}, Ready: ${dmu_ready}"
 
-[ "x${up}" = "xtrue" ] && [ "x${ready}" = "xtrue" ] \
+[ ! -z "${DNSMASQ_SERVER_MODE+set}" ] \
+	&& . /healthcheck-dnsmasq.sh
+
+[ "x${dmu_up}" = "xtrue" ] && [ "x${dmu_ready}" = "xtrue" ] \
 	&& exit 0
 
 exit 1
